18ba02cf1244277a9a4098b72fbbce897a9c61f7
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-02-10 11:38:32 +0100

    100225 variabili

---
+++ js/100225/index.js
@@ -0,0 +1,20 @@
+let number1 = 4;
+let number2 = 8;
+
+const numeroStudenti = 6;
+
+// Altro codice
+
+number1 = "16";
+
+let nome = "Giacomo";
+let cognome = "La Spinà";
+
+let eta = "20";
+eta = 21;
+
+let staPiovendo = true;
+
+let staNevicando = false;
+
+console.log("ciao come stai?", staPiovendo, staNevicando)
