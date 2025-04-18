c3152193bcfe62bccd7193f18a578d4a513c7350
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-04-18 11:17:27 +0200

    180425 esercizio ripasso

--- react/first-react-app/src/esercizioRipassoFormListe/esercizioRipassoFormList.jsx
+++ react/first-react-app/src/esercizioRipassoFormListe/esercizioRipassoFormList.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 
+const initFormData = {
+  name: "",
+  email: "",
+  birthday: "",
+  gender: "",
+  premium: false,
+};
+
 const EsercizioRipassoFormListe = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    birthday: "",
-    gender: "",
-    premium: false,
-  });
+  const [formData, setFormData] = useState(initFormData);
 
   const [utenti, setUtenti] = useState([]);
+
+  const [filtro, setFiltro] = useState("tutti");
 
   const handleChange = (event) => {
     setFormData({
@@ -23,11 +27,16 @@ const EsercizioRipassoFormListe = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setUtenti([...utenti, formData]);
+    if (formData.name) {
+      setUtenti([...utenti, formData]);
+      setFormData(initFormData);
+    } else {
+      alert("Il campo nome è obbligatorio!");
+    }
   };
 
   useEffect(() => {
-    console.log("Lista utenti è stata aggiornata!", utenti)
+    console.log("Lista utenti è stata aggiornata!", utenti);
   }, [utenti]);
 
   return (
@@ -50,6 +59,7 @@ const EsercizioRipassoFormListe = () => {
         <input
           name="birthday"
           type="date"
+          max="2003-01-01"
           value={formData.birthday}
           onChange={handleChange}
         />
@@ -69,9 +79,32 @@ const EsercizioRipassoFormListe = () => {
           />
           <label for="premium">Premium</label>
         </div>
-
         <button type="submit">Registra</button>
       </form>
+      <div className="my-4 border-t-1">
+        <h4>Filtri</h4>
+        <div>Filtro applicato: {filtro}</div>
+        <div className="flex gap-4">
+          <button onClick={() => setFiltro("premium")} className={filtro === "premium" && "!bg-amber-500"}>Premium</button>
+          <button onClick={() => setFiltro("base")} className={filtro === "base" && "!bg-amber-500"}>Base</button>
+          <button onClick={() => setFiltro("tutti")} className={filtro === "tutti" && "!bg-amber-500"}>Tutti</button>
+        </div>
+      </div>
+      <ul className="my-4 border-t-1 flex gap-4 items-center justify-center">
+        {utenti.filter((value) => {
+          if (filtro === "premium") {
+            return value.premium;
+          }
+          if (filtro === "base") {
+            return !value.premium;
+          }
+          return value;
+        }).map((value, index) => (
+          <li key={index} className={value.premium && "text-blue-500"}>
+            {value.name}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
