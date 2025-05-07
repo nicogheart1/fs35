8818b59aa08f50eeeed0e90c2bf6601440b1c277
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-05-07 11:25:35 +0200

    07052025 useSWR

--- react/first-react-app/src/pages/ListaProdotti.jsx
+++ react/first-react-app/src/pages/ListaProdotti.jsx
@@ -1,34 +1,61 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import { Outlet, useNavigate } from "react-router-dom";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const ListaProdotti = () => {
-  const [prodotti, setProdotti] = useState([]);
-
   const navigate = useNavigate();
 
-  const getProductList = async () => {
-    try {
-      const response = await fetch("https://api.sampleapis.com/beers/ale");
-      const data = await response.json();
-      setProdotti(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const {
+    data: prodotti = [],
+    error,
+    isLoading: loading,
+    mutate: updateProdotti,
+  } = useSWR("https://api.sampleapis.com/beers/ale", fetcher);
 
-  useEffect(() => {
-    getProductList();
-  }, []);
+  // const [prodotti, setProdotti] = useState([]);
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+
+  // const getProductList = async () => {
+  //   try {
+  //     setError(false);
+  //     setLoading(true);
+  //     const response = await fetch("https://api.sampleapis.com/beers/ale");
+  //     const data = await response.json();
+  //     setProdotti(data);
+  //   } catch (error) {
+  //     console.error(error);
+  //     setError(true);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getProductList();
+  // }, []);
 
   return (
     <div>
       <h1>Prodotti</h1>
 
       <Outlet />
-    
+
+      {error && (
+        <div className="text-red-500">
+          Si è verificato un errore! Riprova più tardi
+        </div>
+      )}
+
+      {loading && <div>Caricamento in corso...</div>}
+
+      <button onClick={() => updateProdotti()}>Aggiorna prodotti</button>
+
       <div className="flex flex-row flex-wrap gap-4 my-8">
-        {prodotti.slice(0,15).map((product) => (
+        {prodotti.slice(0, 15).map((product) => (
           <Card
             key={product.id}
             text={product.name}
