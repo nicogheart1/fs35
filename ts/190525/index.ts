f28c99b67c8307c1831b4221448e0c8d2febc011
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-05-20 08:53:13 +0200

    190525 ts primitive

---
+++ ts/190525/index.ts
@@ -0,0 +1,18 @@
+/*let numero: string = props.age;
+numero = 16
+
+numero.toUpperCase();*/
+
+
+let numero: number = 1;
+numero = 2;
+
+let text: string = "ciao";
+text = "come stai"
+
+let controllo: boolean = true;
+controllo = false;
+
+let non_definito: undefined;
+
+let valore_nullo: null;
