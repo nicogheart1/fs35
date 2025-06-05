095fd0bdd44f2f8bd15451514294826c0a673f70
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-06-05 11:14:14 +0200

    050625 commonjs modulejs

---
+++ node/050625/modulo.js
@@ -0,0 +1,10 @@
+function somma(a, b) {
+    console.log(`La somma è: ${a + b}`);
+}
+
+function sottrazione(a,b) {
+    console.log(`La sottrazione è: ${a-b}`);
+}
+
+export default { somma, sottrazione };
+// module.exports = somma;
---
+++ node/050625/server.js
@@ -0,0 +1,5 @@
+import { somma } from "./modulo.js";
+
+// const somma = require("./moduloEsterno");
+
+somma(2, 7);
